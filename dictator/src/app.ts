632259152e3747a763dd3028986c90395eb0f9dc
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-02-17 23:45:13 +0000

    added the subscription

--- dictator/src/app.ts
+++ dictator/src/app.ts
@@ -15,16 +15,16 @@ console.log(config);
     });
 
     // Consume a message
-    // const subscription = await broker.subscribe('s1');
-    // subscription
-    //   .on('message', (message, content, ackOrNack) => {
-    //     console.log(content);
-    //     ackOrNack();
-    //   })
-    //   .on('error', (err) => {
-    //   console.log("here is the error");
-    //   console.error()
-    // });
+    const subscription = await broker.subscribe('s1');
+    subscription
+      .on('message', (message, content, ackOrNack) => {
+        console.log(content);
+        ackOrNack();
+      })
+      .on('error', (err) => {
+      console.log("here is the error");
+      console.error()
+    });
   } catch (err) {
     console.log("here is the error");
     console.error(err);

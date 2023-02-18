bce3def9a0e245edf351c316dd2050501bda1157
Dhanush Thakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2023-02-18 01:47:05 +0000

    9 feature/dictator/db setup (#10)
    * added db
    
    * added inital postgres setup
    
    * added pg
    
    * done with intial database and model setup

---
+++ dictator/db/setup.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('postgres', 'sanju', 'Dhanush97', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+export default sequelize;

31b47dac370ec70135263fa55a14f672df94d974
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-02-18 12:42:54 +0000

    updated the files

---
+++ follower/tests/init.spec.ts
@@ -0,0 +1,13 @@
+import * as chai from "chai";
+import sinonChai from "sinon-chai";
+import chaiAsPromised from "chai-as-promised";
+const {expect} = chai;
+chai.use(sinonChai)
+chai.use(chaiAsPromised);
+
+
+describe("INIT", () => {
+    it("should execute this test", () => {
+            expect("hii").to.be.equal("hii")
+    })
+})

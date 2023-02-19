3f3280ad2b3046b75e591fc2c095cc1db747f55c
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-02-19 01:11:08 +0000

    rolled back changes

--- dictator/src/app.ts
+++ dictator/src/app.ts
@@ -1,13 +1,13 @@
-// import {BrokerAsPromised as Broker, withDefaultConfig} from "rascal";
+import {BrokerAsPromised as Broker, withDefaultConfig} from "rascal";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import config from "../rascalConfig.ts";
-// import Run from "../db/run";
+import Run from "../db/run";
+
 import express from "express";
-// import * as Process from "process";
 const app = express();
-// console.log(Run);
+console.log(Run);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
@@ -18,28 +18,28 @@ app.listen(3000, () => {
 });
 
 
-// (async () => {
-//   try {
-//     const broker = await Broker.create(withDefaultConfig(config));
-//     console.log("Connected!");
-//     broker.on('error', (err) => {
-//       console.log("here is the error");
-//       console.error()
-//     });
-//
-//     // Consume a message
-//     const subscription = await broker.subscribe('s1');
-//     subscription
-//       .on('message', (message, content, ackOrNack) => {
-//         console.log(content);
-//         ackOrNack();
-//       })
-//       .on('error', (err) => {
-//       console.log("here is the error");
-//       console.error()
-//     });
-//   } catch (err) {
-//     console.log("here is the error");
-//     console.error(err);
-//   }
-// })();
+(async () => {
+  try {
+    const broker = await Broker.create(withDefaultConfig(config));
+    console.log("Connected!");
+    broker.on('error', (err) => {
+      console.log("here is the error");
+      console.error()
+    });
+
+    // Consume a message
+    const subscription = await broker.subscribe('s1');
+    subscription
+      .on('message', (message, content, ackOrNack) => {
+        console.log(content);
+        ackOrNack();
+      })
+      .on('error', (err) => {
+      console.log("here is the error");
+      console.error()
+    });
+  } catch (err) {
+    console.log("here is the error");
+    console.error(err);
+  }
+})();
